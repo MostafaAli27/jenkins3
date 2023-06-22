b65ec9e76e61a423ff89c792f27334dbbed5a9cb
nour mohamed <88977873+nourmohamed99@users.noreply.github.com>
2023-06-23 00:58:36 +0300

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,5 +1,36 @@
-#!/usr/bin/env groovy
+pipeline {
+     agent any
+    parameters {
+        booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
+        choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
+    }
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+    stages {
+        stage('check') {
+            steps {
+                echo "checking your code"
+                echo "${params.namespace}"
+               
+            }
+        }
+
+        stage('test') {
+            when {
+                expression{
+                    params.project == true 
+                }
+            }
+            steps {
+                echo "testing your app" 
+            }
+        }
+        
+        stage('deployment') {  
+            steps {
+                echo "your code is deployed right now"
+                echo "this build number $BUILD_NUMBER"
+            }
+        }    
+    }
+
 }
